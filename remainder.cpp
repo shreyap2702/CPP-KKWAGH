a1f62c998a797344c8762dfa9603ea4377acf016
Shreya Pandey <shreyapandey@Shreyas-MacBook-Air.local>
2023-04-13 19:15:51 +0530

    seventh cpp code

---
+++ remainder.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ int num1,quot,remainder,num2;
+ 
+
+ cout<<"Enter the number 1:";
+ cin>>num1;
+ 
+ 
+ cout<<"Enter the number 2:";
+ cin>>num2;
+ 
+ 
+ 
+ quot=num1/num2;
+ 
+ cout<<"Quotient is:"<<quot<<endl;
+ 
+ remainder=num1%num2;
+ 
+ cout<<"Remainder is:"<<remainder<<endl;
+ 
+ return 0;
+ }
