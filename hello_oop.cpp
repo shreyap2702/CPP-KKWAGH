4c1dee10fea5abe2740b690cbe82992ef7aae68c
Shreya Pandey <shreyapandey@Shreyas-MacBook-Air.local>
2023-04-13 19:23:22 +0530

    eighth cpp code

---
+++ hello_oop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class temp
+{
+ public:
+         int display()
+          {
+            cout<<"Hello World"<<endl;
+            return 0;
+          }
+ };
+ 
+ int main()
+ {
+  temp obj;
+  obj.display();
+  return 0;
+ } 
